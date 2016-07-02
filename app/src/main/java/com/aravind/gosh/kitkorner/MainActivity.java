0273a561023ad4e0e85aac4b0bda2f27b1516cbd
aravinth2094 <gosh.aravind@gmail.com>
2016-07-03 01:56:28 +0530

    Chronometer code, tested in 5.1.1

--- app/src/main/java/com/aravind/gosh/kitkorner/MainActivity.java
+++ app/src/main/java/com/aravind/gosh/kitkorner/MainActivity.java
@@ -1,7 +1,13 @@
 package com.aravind.gosh.kitkorner;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
+        final ToggleButton button = (ToggleButton) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(button.isChecked()) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    chronometer.start();
+                    Toast.makeText(chronometer.getContext(), "Started", Toast.LENGTH_SHORT).show();
+                } else {
+                    chronometer.stop();
+                    Toast.makeText(chronometer.getContext(), "Stopped", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
