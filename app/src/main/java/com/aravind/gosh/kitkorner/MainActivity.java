5aefef2a7c21c3c4d4aa640857a13e6020dbf294
aravinth2094 <gosh.aravind@gmail.com>
2016-07-03 14:12:10 +0530

    Base64 encoding and decoding

--- app/src/main/java/com/aravind/gosh/kitkorner/MainActivity.java
+++ app/src/main/java/com/aravind/gosh/kitkorner/MainActivity.java
@@ -1,13 +1,14 @@
 package com.aravind.gosh.kitkorner;
 
-import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,20 +16,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
-        final ToggleButton button = (ToggleButton) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button encodeButton = (Button) findViewById(R.id.encodeButton);
+        final Button decodeButton = (Button) findViewById(R.id.decodeButton);
+        final EditText inputText = (EditText) findViewById(R.id.inputText);
+        final TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+        final Switch switcher = (Switch) findViewById(R.id.switcher);
+        encodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(button.isChecked()) {
-                    chronometer.setBase(SystemClock.elapsedRealtime());
-                    chronometer.start();
-                    Toast.makeText(chronometer.getContext(), "Started", Toast.LENGTH_SHORT).show();
-                } else {
-                    chronometer.stop();
-                    Toast.makeText(chronometer.getContext(), "Stopped", Toast.LENGTH_SHORT).show();
+                try {
+                    if(switcher.isChecked()) {
+                        resultTextView.setText(asciiToHex(encode(hexToASCII(inputText.getText().toString()))));
+                    } else {
+                        resultTextView.setText(asciiToHex(encode(inputText.getText().toString())));
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(switcher.getContext(), "Oops something went wrong\nHere's why: "+e.getMessage(), Toast.LENGTH_LONG).show();
                 }
+
             }
         });
+        decodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    if(switcher.isChecked()) {
+                        resultTextView.setText(asciiToHex(decode(hexToASCII(inputText.getText().toString()))));
+                    } else {
+                        resultTextView.setText(asciiToHex(decode(inputText.getText().toString())));
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(switcher.getContext(), "Oops something went wrong\nHere's why: "+e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    private String encode(String inputToEncode) {
+        return Base64.encodeToString(inputToEncode.getBytes(), Base64.CRLF);
+    }
+
+    private String decode(String inputToDecode) {
+        return new String(Base64.decode(inputToDecode.getBytes(), Base64.CRLF));
+    }
+
+    private static String asciiToHex(String asciiValue) {
+        char[] chars = asciiValue.toCharArray();
+        StringBuffer hex = new StringBuffer();
+        for (int i = 0; i < chars.length; i++) {
+            hex.append(Integer.toHexString((int) chars[i]) + " ");
+        }
+        return hex.toString();
+    }
+
+    private static String hexToASCII(String hexValue) {
+        StringBuilder output = new StringBuilder("");
+        for (int i = 0; i < hexValue.length(); i += 2)
+        {
+            String str = hexValue.substring(i, i + 2);
+            output.append((char) Integer.parseInt(str, 16) + " ");
+        }
+        return output.toString();
     }
 }
